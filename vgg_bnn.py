ef7213ba70b3651b2da03c11a1d21dc5024cc6f4
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-11-26 19:45:29 +0800

    First commit

---
+++ vgg_bnn.py
@@ -0,0 +1,97 @@
+import numpy as np
+import tensorflow as tf
+import keras
+from tensorflow.python.keras.optimizers import Adadelta
+from tensorflow.python.keras.models import Model
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from tensorflow.python.keras.callbacks import CSVLogger
+csv_logger = CSVLogger('log.csv', append=True, separator=';')
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ''
+        frozen_graph = tf.graph_util.convert_variables_to_constants(
+            session, input_graph_def, output_names, freeze_var_names)
+        return frozen_graph
+num_classes = 10
+(x_train, y_train), (x_test, y_test) = tf.contrib.keras.datasets.mnist.load_data()
+x_train = x_train.reshape(-1, 28, 28, 1).astype('float32')
+x_test = x_test.reshape(-1, 28, 28, 1).astype('float32')
+x_train /= 255
+x_test /= 255
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+img_input = tf.keras.layers.Input(shape=(28,28,1))
+# 1st block
+x = BNN2D(64, (3, 3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = BNN2D(64, (3, 3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+# 2nd block
+x = BNN2D(128, (3,3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = BNN2D(128, (3,3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+# 3rd block
+x = BNN2D(256, (3,3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = BNN2D(256, (3,3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = BNN2D(256, (3,3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+# 4th block
+x = BNN2D(512, (3,3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = BNN2D(512, (3,3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = BNN2D(512, (3,3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+# 5th block
+x = BNN2D(512, (3,3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = BNN2D(512, (3,3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+x = BNN2D(512, (3,3))(img_input)
+x = BatchNormalization()(x)
+x = Activation(binary_tanh)(x)
+# output
+x = tf.keras.layers.Flatten()(x)
+x = BinaryDense(4096)(x)
+x = BinaryDense(4096)(x)
+x = BinaryDense(10)(x)
+model = Model(img_input, x, name='vgg16')
+model.summary()
+model.compile(optimizer=Adadelta(),loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, batch_size=128, epochs=15, verbose=1, validation_data=(x_test, y_test), shuffle=True,  callbacks=[csv_logger])
+print('inputs: ', [input.op.name for input in model.inputs])
+print('outputs: ', [output.op.name for output in model.outputs])
+score = model.evaluate(x_test, y_test, verbose=1)
+model.save('./vgg.h5')
+frozen_graph = freeze_session(tf.keras.backend.get_session(), output_names=[out.op.name for out in model.outputs])
+tf.train.write_graph(frozen_graph, './', 'vgg.pbtxt', as_text=True)
+tf.train.write_graph(frozen_graph, './', 'vgg.pb', as_text=False)
